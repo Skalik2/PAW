6c76014635ebc1b677208ce5ff2e5fe64c156e3c
Skalik <106670239+Skalik2@users.noreply.github.com>
2024-10-02 09:26:47 +0000

    Cm1_1

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 (function () {
-  //TODO
+  let ex1_button = document.getElementById("ex1_button");
+  let ex1_content = document.getElementById("ex1_content");
+
+  let numbers = document.createElement("p");
+  numbers.textContent = "0,1,2,3,4,5,6,7,8,9";
+  console.log(numbers);
+  ex1_button.addEventListener("click", () => {
+    ex1_content.appendChild(numbers);
+  });
 })();

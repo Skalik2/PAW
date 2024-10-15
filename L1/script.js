a45bbbea332e8d8f53fb601b82327d3c88c74966
Skalik <106670239+Skalik2@users.noreply.github.com>
2024-10-16 00:10:10 +0200

    Cm1_2
    - added dragable elements
    - adjusted folder structure

--- L1/script.js
+++ L1/script.js
@@ -1,8 +1,12 @@
 (function () {
-  let ex1_button = document.getElementById("ex1_button");
-  let ex1_content = document.getElementById("ex1_content");
-  let ex2_text = document.getElementById("ex2_text");
-  let ex2_content = document.getElementById("ex2_content");
+  const ex1_button = document.getElementById("ex1_button");
+  const ex1_content = document.getElementById("ex1_content");
+  const ex2_text = document.getElementById("ex2_text");
+  const ex2_content = document.getElementById("ex2_content");
+
+  const contener1 = document.getElementById("ex3_one");
+  const contener2 = document.getElementById("ex3_two");
+  const draggableElement = document.getElementById("ex3_element");
 
   let numbers = document.createElement("p");
   numbers.textContent = "0,1,2,3,4,5,6,7,8,9";
@@ -26,4 +30,20 @@
     }
     if (!ifError) ex2_content.textContent = "Numer telefonu jest poprawny";
   });
+
+  draggableElement.addEventListener("dragstart", ()=>{
+     contener2.addEventListener("dragover", (e)=>{
+        e.preventDefault();
+     })
+     contener2.addEventListener("drop", ()=>{
+      contener2.appendChild(draggableElement);
+     })
+
+     contener1.addEventListener("dragover", (e)=>{
+      e.preventDefault();
+   })
+   contener1.addEventListener("drop", ()=>{
+    contener1.appendChild(draggableElement);
+   })
+  })
 })();
